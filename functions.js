c3c6672e34f82bffa36dbb6e8c9d2f50cd7a2209
adridlv <adriandlv53@gmail.com>
2016-06-17 13:44:52 +0200

    refactorizado otra vez

--- functions.js
+++ functions.js
@@ -1,9 +1,3 @@
-"use strict";
-
-var minVikingHealth = 5;
-var maxVikingHealth = 20;
-var minVikingStrength = 4;
-var maxVikingStrength = 7;
 
 
 //Función constructor Vikingo
@@ -18,30 +12,10 @@ Viking.prototype.hurt = function(damage){
 	this.health -= damage;
 }
 
-Viking.prototype.hit = function(defender){
-	var damage = this.generateDamage();
-	var healthDefender = defender.health;
-	var fightFinished = false;
-
-	console.log("Damage Attacker: "+ damage + " Health Defender: "+healthDefender);
-
-	if(healthDefender - damage > 0){
-		defender.hurt(damage);
-		console.log("New health defender: "+ defender.health);
-	}
-	else{
-		console.log("Training finished");
-		fightFinished = true;
-	}	
-	
-	return fightFinished;
-}
-
 Viking.prototype.generateDamage = function () {
 	var damage = generateRandom(this.strength-2, this.strength+2);
 	return damage;
 }
-
 
 //Función constructor Saxon 
 var Saxon = function (health, strength, defense) {
@@ -61,6 +35,32 @@ Saxon.prototype.hurt = function(damage){
 	this.health -= damage;
 }
 
+function fight(character1, character2, training){
+	var finish = false;
+	var turn;
+	var training;
+	var winner = 0;
+
+	if(training)
+		console.log("////////////////START TRAINING////////////////");	
+	else
+		console.log("////////////////START FIGHTING////////////////");	
+
+	while(winner == 0){
+		turn = generateRandom(1,2);
+
+		if(turn === 1){
+			winner = turnHit(character1,character2,training);
+		}
+		else if(turn === 2){
+			winner = turnHit(character2,character1,training);
+		}	
+		
+	}
+
+	return winner;
+
+}
 
 //Función constructor Pit
 var Pit = function(viking1, viking2){
@@ -69,61 +69,62 @@ var Pit = function(viking1, viking2){
 }
 
 //Prototipos de Pit
-Pit.prototype.vikingTraining = function(){
+Pit.prototype.startFight = function(){
 	var finish = false;
 	var turn;
+	var training = false;
 
-	console.log("////////////////START TRAINING////////////////");
-
-	while(!finish){
-		turn = generateRandom(1,2);
-
-		if(turn === 1){
-			finish = turnHitTraining(this.viking1,this.viking2);
-		}
-		else if(turn === 2){
-			finish = turnHitTraining(this.viking2,this.viking1);
-		}
-		
-	}
+	fight(this.viking1,this.viking2,true);
 }
 
-//Función constructor Fight
-var Fight = function(){
+//Función constructor War
+var War = function(){
 	this.vikingArray = [],
 	this.saxonArray = [];
 }
-
-//Prototipos de Fight
-Fight.prototype.generateArmy = function(){
-
-	var randomVikingLength = generateRandom(20,30);
-	var randomSaxonLength = generateRandom(40, 60);
-
-	for(var i = 0; i<randomVikingLength; i++){
-		var viking = createViking();
-		this.vikingArray.push(viking);
-	}
-
-	for(var j = 0; j<randomSaxonLength; j++){
-		var saxon = createSaxon();
-		this.saxonArray.push(saxon);
+//function createSoldier (name, penalty, type){
+function generateArray(type,array,arrayLength, penalty=0 ){
+	for(var i = 0; i<arrayLength; i++){
+		if(type == "viking")
+			var obj = createSoldier(undefined,0, "viking");
+		else
+			var obj = createSoldier(undefined, penalty, "saxon");
+		array.push(obj);
 	}
 }
 
-Fight.prototype.battle = function(){
-	var turn = generateRandom(5,8);
-	var indexArray = 0;
+//Prototipos de War
+War.prototype.generateArmy = function(){
 
-	for(var actualTurn = 0; actualTurn<turn; actualTurn++){
-		var viking = this.vikingArray[indexArray];
-		var saxon = this.saxonArray[indexArray];
-		fighting(viking,saxon);
-		indexArray++;
+	var randomVikingLength = generateRandom(100,150);
+	var randomSaxonLength = generateRandom(300, 600);
+
+	generateArray("viking", this.vikingArray, randomVikingLength,0);
+	generateArray("saxon", this.saxonArray, randomSaxonLength,2);
+}
+
+War.prototype.startFight = function(){
+	var turn = generateRandom(5,8);
+	var indexViking = 0, indexSaxon = 0;
+	var winner = 0;
+
+	while(indexViking<this.vikingArray.length && indexSaxon<this.saxonArray.length){
+		var viking = this.vikingArray[indexViking];
+		var saxon = this.saxonArray[indexSaxon];
+		winner = fight(viking,saxon,false);
+		console.log(winner);
+		if(winner==1){
+			indexSaxon+=1;
+			console.log("Saxon dead "+indexSaxon);
+		}
+		else if(winner == 2){
+			indexViking+=1;
+			console.log("Viking dead " +indexViking);
+		}
 	}		
 }
 
-Fight.prototype.counter = function (array,character) {
+War.prototype.counter = function (array,character) {
 	var counter = 0;
 
 	for (var i=0; i<array.length; i++) {
@@ -135,123 +136,86 @@ Fight.prototype.counter = function (array,character) {
 	console.log(counter + " "+ character + " are dead!");
 }
 
-
-
 function generateRandom(min, max){
 	return Math.floor(Math.random() * (max-min) + min);
 }
 
-function createViking (vikingName) {
-	var name = vikingName || "Viking";
-	var health = generateRandom(minVikingHealth,maxVikingHealth);
-	var strength = generateRandom(minVikingStrength,maxVikingStrength);
-	var viking = new Viking (name, health, strength);
-	return viking;
+function createSoldier (name, penalty, type){
+	var health = generateRandom(12-penalty,20-penalty);
+	var strength = generateRandom(4-penalty,6-penalty);
+	var obj;
+
+	if(type == "viking"){
+		var soldierName = name || "Viking";
+		obj = new Viking (soldierName, health, strength);
+	} else {
+		var defense = generateRandom();
+		obj = new Saxon (health, strength);
+	}
+	return obj;
 }
 
-function createSaxon(){
-	var health = generateRandom(minVikingHealth-2 , maxVikingHealth-2);
-	var strength = generateRandom(minVikingStrength-2,maxVikingStrength-2);
-	var defense = generateRandom()
-	var saxon = new Saxon (health, strength);
-	return saxon;
-}
-
-//Pelea sin llegar a matar
-function hitEnemyTraining (attacker, defender) {
+function hitEnemy (attacker, defender, training) {
 	var fightFinished = false;
 	var damage = attacker.generateDamage();
 	var healthDefender = defender.health;
 
-	console.log("===== " + attacker.name +" turn =======");
-	
-	if(healthDefender - damage > 0){
-		console.log("Damage Attacker: " +damage + " Health Defender: " + healthDefender);
+	if(training){
+		console.log("===== " + attacker.name +" turn =======");
+		if(healthDefender - damage > 0){
+			console.log("Damage Attacker: " +damage + " Health Defender: " + healthDefender);
+			defender.hurt(damage);
+			console.log("New health: "+defender.health);
+		}
+		else{
+			console.log("Training finished");
+			fightFinished = true;
+		} 
+	}else{
+
+		console.log("===== " + attacker.name +" turn =======");
+		console.log("Damage Attacker: " + damage + " Health Defender: " + healthDefender);
+
 		defender.hurt(damage);
-		console.log("New health: "+defender.health);
-	}
-	else{
-		console.log("Training finished");
-		fightFinished = true;
-	} 
 
+		if(defender.health <= 0){
+			console.log("Fight finished");
+			fightFinished = true;
+		}
+
+	}
+	
 	return fightFinished;
 }
 
-//Turnos de pelea en el Training(si la pelea aún no ha terminado se intercambian los papeles)
-function turnHitTraining(attacker, defender){
+function turnHit(attacker, defender, training){
 	var fightFinished = false;
+	var winner = 0;
 
-	fightFinished = hitEnemyTraining(attacker,defender);
-	
+	fightFinished = hitEnemy(attacker,defender, training);
+	if(fightFinished) winner = 1;
+
 	if(!fightFinished){
-		fightFinished = hitEnemyTraining(defender,attacker);
+		fightFinished = hitEnemy(defender,attacker, training);
+		if(fightFinished)
+			winner = 2;
 	}
 
-	return fightFinished;
+	return winner;
 }
-
-//Golpea enemigo siempre y cuando la vida no sea menor o igual a 0
-function hitEnemy (attacker, defender) {
-	var fightFinished = false;
-	var damage = attacker.generateDamage();
-	var healthDefender = defender.health;
-
-	console.log("===== " + attacker.name +" turn =======");
-
-	console.log("Damage Attacker: " + damage + " Health Defender: " + healthDefender);
-	
-	defender.hurt(damage);
-	
-	if(defender.health <= 0){
-		console.log("Fight finished");
-		fightFinished = true;
-	}
-
-	return fightFinished;
-}
-
-//Turnos de pelea en la batalla. Termina cuando alguien muere
-function turnAttackFight(attacker, defender){
-	var fightFinished = false;
-	
-	fightFinished = hitEnemy (attacker, defender);
-	if (!fightFinished) {
-		fightFinished = hitEnemy (defender, attacker);
-	}
-
-	return fightFinished;
-
-}
-
-//Decide los turnos. Se ejecuta cada vez que luchan
-function fighting(viking, saxon){
-	var finish = false;
-	var turn;
-
-	while(!finish){
-		turn = generateRandom(1,2);
-
-		if(turn === 1){
-			finish = turnAttackFight(viking, saxon);
-		}
-		else if(turn === 2){
-			finish = turnAttackFight(saxon,viking);
-		}
-	}
-}
-
 
 //LLamadas de funciones 
-var viking1 = createViking("Ragnar");
-var viking2 = createViking("Uhtred");
+var viking1 = createSoldier("Ragnar", 0, "viking");
+var viking2 = createSoldier("Uhtred", 0, "viking");
 
-var pit = new Pit (viking1, viking2);
-pit.vikingTraining();
+var pit = new Pit(viking1, viking2);
+pit.startFight();
 
-var fight = new Fight();
-fight.generateArmy();
-fight.battle();
-fight.counter(fight.vikingArray, "vikings");
-fight.counter(fight.saxonArray, "saxons");
+var war = new War();
+war.generateArmy();
+
+war.startFight();
+war.counter(war.vikingArray, "vikings");
+war.counter(war.saxonArray, "saxons");
+
 
